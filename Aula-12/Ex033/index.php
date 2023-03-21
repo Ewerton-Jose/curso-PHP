f881e6747147eebfe933d1767ca0927602658a01
Ewerton J. Silva <76182422+Ewerton-Jose@users.noreply.github.com>
2023-03-21 16:18:07 -0300

    Aula 12 concluída e exercício 33 adicionado

---
+++ Aula-12/Ex033/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+include_once 'includes/header.html';
+
+echo "Tudo certinho";
+
+include_once 'includes/footer.html';
+
+?>
--- Aula-12/aula12.php
+++ Aula-12/aula12.php
@@ -1,5 +1,34 @@
 <?php
 
-/* Comandos importantes envolveldo banco de dados */
+/* Comandos importantes envolveldo banco de dados
+
+
+Mostrar como entra no bd do Xamp
+Explicar parte por parte
+
+Mostrar os comandos que iremos usar no próximo treino
+
+$conexao  = ¹mysqli_connect($server, $user, $password, $dbname);
+    - Guarda a conexão com o BD
+
+$sql = "SELECT * FROM tabela";
+$resultado = ²mysqli_query($conexao, $sql);
+    - Executa um comando SQL que nesse caso seria selecionar todos os itens da tabela escolhida
+
+while($dados = ³mysqli_fetch_array($resultado)){
+        - nesse caso "dados" está pegando uma 'lista sql' e virando uma lista php
+    $nome = $dados['nome'];
+        - Dentro de "dados" você tá pegando o elemento "nome"
+    $sobrenome = $dados['sobrenome'];
+        - Dentro de "dados" você tá pegando o elemento "sobrenome"
+    $idade = $dados['idade'];
+        - Dentro de "dados" você tá pegando o elemento "idade"
+
+    echo "Você é $nome $sobrenome e você tem $idade anos";
+}
+
+EX033: Crie um BD e mostre na tela de usuários os dados que tem nele, que vão ser Nome, Idade, Email, Telefone, id (o exércício deve conter includes)
+
+*/
 
 ?>
