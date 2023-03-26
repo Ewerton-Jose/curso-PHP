fcc6e80125e5ebf654ad8fd467f34c3aee3e452d
Ewerton J. Silva <76182422+Ewerton-Jose@users.noreply.github.com>
2023-03-25 21:55:26 -0300

    EX33 concluído
    Exercício linkado com o bd

--- Aula-12/Ex033/index.php
+++ Aula-12/Ex033/index.php
@@ -1,8 +1,21 @@
 <?php 
 
+require_once 'php-actions/connect.php';
 include_once 'includes/header.html';
 
-echo "Tudo certinho";
+$sql = "SELECT * FROM pessoa";
+$resultado = mysqli_query($conexao,$sql);
+
+while ($dados = mysqli_fetch_array($resultado)){
+
+echo 'Pessoa '.$dados['id'].': <br>';
+
+echo $dados['nome'].'<br>';
+echo $dados['email'].'<br>';
+echo $dados['telefone'].'<br>';
+echo $dados['idade'].'<br>.<br>';
+};
+
 
 include_once 'includes/footer.html';
 
---
+++ Aula-12/Ex033/php-actions/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$user = "root";
+$server = "localhost";
+$senha = "";
+$dbname = "pessoa";
+
+$conexao =   mysqli_connect($server, $user, $senha, $dbname);
+
+
+?>
